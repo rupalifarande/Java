e90066f62d2d3133de5b9163f10df050c614d4c6
rupalifarande <rupalifarande28199@gmail.com>
2021-04-02 21:44:05 +0530

    this is my eclipse first commit

---
+++ MyName/src/my/farande.java
@@ -0,0 +1,14 @@
+package my;
+
+public class farande {
+
+	public static void main(String[] args) {
+		int num = 500_00_00_00;
+		num = 8;
+		System.out.print(num);
+
+		
+
+	}
+
+}
---
+++ MyName/src/my/ra.java
@@ -0,0 +1,15 @@
+package my;
+
+public class ra {
+
+	public static void main(String[] args) 
+	{
+	int	a = 5;
+	int b = 4;
+	int c;
+	
+   System.out.print(a + b);
+		    
+	}
+
+}
